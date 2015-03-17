c681871639a9f5e990b2b7ef19e7775724d90487
ryu-s <rysestock@gmail.com>
2015-03-17 18:54:47 +0900

    追加忘れを追加

---
+++ Irc4Control/MyDataTable.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Irc4Control
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MyDataTable : DataTable
+    {
+        List<Irc4.Log> logList = new List<Irc4.Log>();
+        string column1Name = "text";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public MyDataTable()
+        {
+            this.Columns.Add(column1Name);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="log"></param>
+        public void SetLog(Irc4.Log log)
+        {
+            var newRow = this.NewRow();
+            newRow[column1Name] = log.Text;
+            this.Rows.Add(newRow);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rowIndex"></param>
+        /// <returns></returns>
+        public Irc4.Log GetLog(int rowIndex)
+        {
+            if (rowIndex < 0 || rowIndex >= logList.Count)
+                throw new ArgumentException("rowIndex");
+            return logList[rowIndex];
+        }
+    }
+}
